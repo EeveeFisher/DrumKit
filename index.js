aec61999f23930d9e16080b3f92a82a044afef22
EeveeFisher <alexzhu7949@gmail.com>
2025-02-18 15:18:56 -0500

    add Keypress listener

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-       var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
+    document.addEventListener("keypress", function(event) {
+       var key = event.key;
+        switch (key) {
             case "w":
                 var tom1 = new Audio("sounds/tom-1.mp3");
                 tom1.play();
